3f9aeae958f040eda0727ab21657db91c64eb7d6
Jenna Han <jenna3@uw.edu>
2016-11-29 10:17:50 -0800

    class

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -15,23 +15,24 @@ var people2 = [
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
   // Create a varaible myDiv by selecting the element with id my-div (this is already created for you).
-
+  var myDiv = d3.select('#my-div');
 
   // Create a variable `paragraphs` by selecting all  `p` elements in  `myDiv`, and binding you data  `people` to it.
   // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
-
+  var paragraphs = myDiv.selectAll('p').data(people, function(p){return p.name});
 
 
   // Add a paragraph to the div for each person in your dataset following instructions in the index.html file
-
+  paragraphs.enter().append('p').text(function(p){return p.name}).style('color', function(p){return p.favColor});
 
   // Use `setTimeout` function to wait 3 seconds, then do the following
   setTimeout(function(){
     // Re-define your variable `paragraphs` by selecting all `p` elements in `myDiv`, and binding the data `people2` to it.
     // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
-
+    paragraphs = myDiv.selectAll('p').data(people2, function(p){return p.name});
 
     // Determine which people are no longer represented in the data (`exit`), and transition their opacity to 0 and remove them.
+    paragraphs.exit().transition().style('opacity', '0').remove();
     
   }, 3000);
 });
